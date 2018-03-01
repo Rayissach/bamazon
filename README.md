274685c8fdfc1823b4983f6ea1834bca3f39f343
rayissach <rayissach@gmail.com>
2018-02-28 19:47:04 -0800

    custumer js completed

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bamazon
+# Bamazon
+
+Bamazon is a one stop shop for all of your daily items. This is an e-commerce store that allows the user to choose an item using an ID number. When chosen the user gets prompted to choose how much of the item they want. If the the stock is available the customer has successfuly purchased the product. If that amount isn't installed the user will be prompted again. The user also has the option to "quit" whenever they tap the q key.
+
+# Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+Prerequisites
+npm install mysql
+npm install inquirer
+npm install cli-table
+
+# Demo
+![Demonstration of my application Bamazon](assets/images/bamazonDemo.gif)
+
+# Built With
+Javascript - contains all of the logic
+Node - Server
+CLI - Table design
+SQL - Database
+
+
+# Authors
+Raymond Robinson
+
+# License
+This project is licensed under the MIT License - see the LICENSE.md file for details
--- bamazon.js
+++ bamazon.js
@@ -1,7 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require('cli-table');
+var Table = require("cli-table");
 
+var counter = 0;
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -20,6 +21,7 @@ connection.connect(function(err) {
 });
 
 
+
 function readProducts() {
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
@@ -29,6 +31,7 @@ function readProducts() {
 
         });
         for (var i = 0; i < res.length; i++) {
+            counter++;
             table.push(
                 [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             );
@@ -37,101 +40,67 @@ function readProducts() {
         buyProduct();
 
     });
-
-
 }
 
-function buyProduct(res) {
+function buyProduct() {
 
-        inquirer.prompt({
+    inquirer.prompt([{
 
             name: "item",
             type: "prompt",
-            message: "What is the Id of the item you would like to purchase?",
-            validate: function(value, res) {
-            	for (var i = 0; i < res.length; i++) {
-                if (isNaN(value) == false && parseInt(value) <= value.length && parseInt(value) > 0) {
+            message: "What is the Id of the item you would like to purchase? [Hit Q to Quit]",
+            validate: function(value) {
+                if (isNaN(value) === false && parseInt(value) <= counter && parseInt(value) > 0) {
                     return true;
+                } else if (value == "Q" || value === "q") {
+                    console.log("\nThank you for shopping with us!");
+                    process.exit();
                 } else {
-                    return false;
+                    return "Please enter a valid item ID!";
                 }
             }
-
-        }, {
+        },
+        {
             name: "quantity",
             type: "prompt",
-            message: "How many would you like?",
+            message: "How many would you like?[Hit Q to Quit]",
             validate: function(value) {
                 if (isNaN(value)) {
                     return false;
                 } else {
                     return true;
                 }
-     }
-
-        }).then(function(answer) {
-            var uniqueId = (answer.id) - 1;
-            var quantityNum = parseInt(answer.stock_quantity);
-            var Total = parseFloat(((res[uniqueId].price) * quantityNum).toFixed(2));
-            if (res[uniqueId].stock_quantity >= quantityNum) {
-                connection.query("UPDATE Products SET ? WHERE ?", [
-                        { stock_quantity: (res[uniqueId].stock_quantity - quantityNum) },
-                        { ItemId: answer.id }
-                    ],
-                    function(err, res) {
-                        if (err) throw err;
-                        console.log("Yayyy!")
-                    });
-
-                connection.query("SELECT * FROM Departments", function(err, res) {
-                    if (err) throw err;
-                    var index;
-                    for (var i = 0; i < res.length; i++) {
-                        if (res[i].department_name === res[uniqueId].department_name) {
-                            index = i
-                        }
-                    }
-
-                    connection.query("UPDATE Departments SET ? WHERE ?", [
-                        { department_name: res[uniqueId].department_name }
-                    ], function(err, res) {
-                        if (err) throw err;
-                    });
-                });
-            } else {
-                console.log("Sorry");
             }
-            readProducts();
-        })
+        }
+    ]).then(function(answer) {
+        console.log(answer)
+        // var uniqueId = (answer.item) - 1;
+        var quantityNum = parseInt(answer.quantity > 0);
+
+        // if (res[uniqueId].quantity >= quantityNum) {
+        // use answer.item in select to grab item from db
+        connection.query("SELECT * FROM Products WHERE ?", { id: answer.item }, function(err, res) {
+            if (err) throw err;
+            // console.log("cool",res);
+            if (answer.quantity >= res[0].stock_quantity) {
+
+                console.log("Sorry Insufficient availability for the amount you want to purchase...");
+                // buyProduct();
+            } else {
+                connection.query("UPDATE Products SET ? WHERE ?", [
+                        { stock_quantity: (res[0].stock_quantity - answer.quantity) },
+                        { id: answer.item }
+                    ],
+                    function(err) {
+                        if (err) throw err;
+
+                        console.log("\nCongratulations! You now have " + answer.quantity + " of " + res[0].product_name + " in your cart.");
+                    }
+                );
+            }
+
+        });
+        readProducts();
+    });
 
 }
-
-
-// The first should ask them the ID of the product they would like to buy.
-// The second message should ask how many units of the product they would like to buy.
-
-// for (var i = 0; i < res.length; i++) {
-// 	if (res[i].id) {
-// 		console.log("Yep we have everything you need! Your Item was succesfully added.")
-// 	}
-
-///////////////////////////////////////////////////////////////////////////////////////
-          ///////////////////////////////////////////////////
-            //                             validate: function(res) {
-            //                                 for (var i = 0; i < res.length; i++) {
-            //                                     if (res[i].id) {
-            //                                         console.log("Yep we have everything you need! Your Item was succesfully added.");
-            //                                         return;
-            //                                     } else {
-            //                                         console.log("Sorry, we don't have that much inventory... Try again")
-            //                                     }
-            //                                 }
-
-            //                                 /////////////////////////////////////////////////////////
-
-            //                                 .then() var uniqueId;
-            //                                 for (var i = 0; i < res.length; i++) {
-            //                                     if (res[i].id === answer.item) {
-            //                                         uniqueId = res[i];
-            //                                     }
-            //                                 }
